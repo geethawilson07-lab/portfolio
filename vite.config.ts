966fb88aeff2e403f693b8be3c34f832e5f78524
Geetha Wilson <geethawilson07@gmail.com>
2026-05-30 12:12:55 +0530

    updated vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  tanstackStart: {
+    ssr: false,
+  },
+
   vite: {
     build: {
       outDir: "dist",
     },
-  },
-
-  tanstackStart: {
-    ssr: false,
   },
 });
